e78f415f8f1d70fbfba16487333191833e061d04
Hamish Downer <hamish@aptivate.org>
2015-01-07 14:59:39 +0000

    Do basic tests for all views

--- django/website/main/tests/view_tests.py
+++ django/website/main/tests/view_tests.py
@@ -5,13 +5,21 @@ from django.test.testcases import TestCase
 from lib.fast_dispatch import FastDispatchMixin
 
 
-class HomeViewTests(FastDispatchMixin, TestCase):
+class BasicViewTestsMixin(object):
 
-    def test_home_page_returns_status_code_200(self):
-        response = self.fast_dispatch('home')
+    def test_page_returns_status_code_200(self):
+        response = self.fast_dispatch(self.url_name)
         self.assertEqual(200, response.status_code)
 
-    def test_home_page_renders_without_error(self):
-        response = self.fast_dispatch('home')
+    def test_page_renders_without_error(self):
+        response = self.fast_dispatch(self.url_name)
         response.render()
         # fail if there is an exception
+
+
+class HomeViewTests(FastDispatchMixin, BasicViewTestsMixin, TestCase):
+    url_name = 'home'
+
+
+class ChartsViewTests(FastDispatchMixin, BasicViewTestsMixin, TestCase):
+    url_name = 'charts'
