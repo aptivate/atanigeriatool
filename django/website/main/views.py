4fc2cb15333445d87225f9af9eebe1d2b212c9ff
Hamish Downer <hamish@aptivate.org>
2015-01-12 15:47:17 +0000

    Put chart on home page
    and add placeholder titles for other sections

--- django/website/main/views.py
+++ django/website/main/views.py
@@ -8,6 +8,24 @@ from lscharts.embed import EmbedChartSettings
 # NOT included in main/urls.py - included directly in the root urls.py
 class HomeView(TemplateView):
     template_name = 'main/homepage.html'
+    # TODO: move to settings
+    dataset = "29277fe2981511e4bbe006909bee25eb"
+    domain = "ata.livestories.com"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['food_consumed_chart'] = EmbedChartSettings(
+            dataset=self.dataset,
+            domain=self.domain,
+            variables="Commodity,year",
+            indicators="Value",
+            operation="avg",
+            chart_type="column",
+            legend="true",
+            data_labels="true",
+            text="Percentage of households who consume each foodstuff, in 2010 and 2012",
+        )
+        return context
 
 
 class ChartsView(TemplateView):
