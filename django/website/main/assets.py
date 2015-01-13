65ca2c44c3d574bdc636b4314d9a3c7bf1d9dc91
Hamish Downer <hamish@aptivate.org>
2015-01-13 12:11:20 +0000

    changes to bower packages/assets config

--- django/website/main/assets.py
+++ django/website/main/assets.py
@@ -5,12 +5,13 @@ from django_assets import Bundle, register
 common_css = Bundle(
     Bundle(
         # 'bower_components/bootstrap-sass/assets/stylesheets/_bootstrap.scss',
-        'sass/bootstrap/bootstrap.scss',
+        # 'sass/bootstrap/bootstrap.scss',
         # 'bower_components/bootstrap-material-design/sass/material.scss',
         'sass/main.scss',
         filters='pyscss',
         output='css/main/from-scss.css',
     ),
+    'bower_components/bootstrap/dist/css/bootstrap.css',
     'bower_components/bootstrap-material-design/dist/css/material.css',
     filters='cssmin',
     output='css/main/common.min.css',
@@ -20,7 +21,7 @@ register('main.common_css', common_css)
 common_js = Bundle(
     'bootstrap/ie_version.js',
     'bower_components/jquery/dist/jquery.js',
-    'js/bootstrap.js',
+    'bower_components/bootstrap/dist/js/bootstrap.js',
     # 'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
     'bower_components/bootstrap-material-design/dist/js/material.js',
     filters='jsmin',
