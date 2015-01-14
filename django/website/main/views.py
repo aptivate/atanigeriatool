8ca469f4e3a8cbb674ddebfbc18e423f0fe56bd4
Hamish Downer <hamish@aptivate.org>
2015-01-14 15:51:58 +0000

    refactor to pull out a get_charts() method
    that also allows for multiple charts in the future

--- django/website/main/views.py
+++ django/website/main/views.py
@@ -17,18 +17,23 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context['food_consumed_chart'] = EmbedChartSettings(
-            dataset=DATASETS['nutrition'],
-            domain=DOMAIN,
-            variables="Commodity,year",
-            indicators="Value",
-            operation="avg",
-            chart_type="column",
-            legend="true",
-            data_labels="true",
-            text="Percentage of households who consume each food type, in 2010 and 2012",
-        )
+        context['charts'] = self.get_charts()
         return context
+
+    def get_charts(self):
+        return {
+            'nutrition': EmbedChartSettings(
+                dataset=DATASETS['nutrition'],
+                domain=DOMAIN,
+                variables="Commodity,year",
+                indicators="Value",
+                operation="avg",
+                chart_type="column",
+                legend="true",
+                data_labels="true",
+                text="Percentage of households who consume each food type, in 2010 and 2012",
+            )
+        }
 
 
 class StateFilterView(TemplateView):
