3d143b9174590b404ac0ea55b638829f86e8b4bd
Hamish Downer <hamish@aptivate.org>
2015-01-21 12:20:07 +0000

    change colours used in productivity chart

--- django/website/main/views.py
+++ django/website/main/views.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals, absolute_import
 
 from django.views.generic import TemplateView
 
-from lscharts.embed import EmbedChartSettings
+from lscharts.embed import EmbedChartSettings, DEFAULT_COLORS
 
 # TODO: move to settings?
 DOMAIN = "ata.livestories.com"
@@ -86,8 +86,12 @@ class HomeView(TemplateView):
         return args
 
     def get_productivity_args(self, state, valuechain):
+        productivity_colors = DEFAULT_COLORS[:]
+        productivity_colors[0] = '849E92'
+        productivity_colors[1] = '000000'
         args = self.get_generic_args('productivity')
         args.update({
+            'colors': productivity_colors,
             'variables': ["season"],
             'indicators': ["production", "yield"],
             'operation': "sum",
